c527f2a24547d8f019c7bede72868292dcf86c7d
JerryXmas <OOoysterOO@gmail.com>
2016-04-02 18:01:47 +0800

    index

---
+++ main.js
@@ -0,0 +1,14 @@
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=a3e2b221-75e0-45c1-8f97-75acbd43d613";
+
+function getData(cb) {
+  $.ajax({
+    url : API_PATH,
+    method : "GET",
+    success : function(data) {
+      cb(null, data);
+    },
+    error : function(err) {
+      cb(err);
+    }
+  });
+}
